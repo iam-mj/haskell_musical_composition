5db6c3cc1e306d7fc2088cba46e0687476dcb28d
Maria Verdeș <mariaverdes14@gmail.com>
2025-05-03 14:35:15 +0300

    add score creation launch call

---
+++ src/Visual/CreateScore.hs
@@ -0,0 +1,27 @@
+module Visual.CreateScore where
+    
+import System.Process
+import qualified Data.ByteString.Char8 as BS
+
+import Visual.UploadFile
+import Network.HTTP.Types
+
+endpoint      = "\"https://www.noteflight.com/scores/create?scoreTemplateURL=" :: String
+setImportType = "&scoreImportType=midi\""                                      :: String
+
+createScore :: FilePath -> IO ()
+createScore fileName = do
+  -- upload the file to a file host
+  uploadResult <- uploadRequest fileName
+  case uploadResult of
+    Left error -> putStrLn error
+    Right link -> do
+        print link
+        -- build launch link
+        -- note: urlEncode's first argument is set to true => we encode a part of a query string
+        let linkBS      = BS.pack link
+            encodedLink = BS.unpack $ urlEncode True linkBS 
+            launchLink  = endpoint ++ link ++ setImportType
+        
+        -- launch the score editor
+        callCommand $ "start \"\" " ++ launchLink
--- src/Visual/UploadFile.hs
+++ src/Visual/UploadFile.hs
@@ -3,29 +3,41 @@ module Visual.UploadFile where
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Client.MultipartFormData
+import Network.HTTP.Types
+
 import qualified Data.ByteString.Char8 as BS
-import Network.HTTP.Types.Header (hUserAgent)
+import qualified Data.ByteString.Lazy.Internal as BLI
 import qualified Data.Text as T
 
-fileHost   = "https://0x0.st" :: String
+type Error    = String
+type MaybeURL = Either Error String 
 
-fileKey     = T.pack "file"    :: T.Text
-expiracyKey = T.pack "expires" :: T.Text
+fileHost   = "https://litterbox.catbox.moe/resources/internals/api.php" :: String
 
-expiration = BS.pack "2"           :: BS.ByteString -- 2 hours
-userAgent  = BS.pack "curl/7.68.0" :: BS.ByteString
+reqKey      = T.pack "reqtype"      :: T.Text
+fileKey     = T.pack "fileToUpload" :: T.Text
+expiracyKey = T.pack "time"         :: T.Text
+
+reqType    = BS.pack "fileupload" :: BS.ByteString
+expiration = BS.pack "24h"        :: BS.ByteString
+
+errorMessage = "Error: failed to upload file to host, status = "
 
 
-uploadRequest :: FilePath -> IO ()
+uploadRequest :: FilePath -> IO MaybeURL
 uploadRequest fileName = do
   manager <- newManager tlsManagerSettings
 
   -- build the request
   initialRequest <- parseRequest $ "POST " ++ fileHost
-  let requestHeaders = initialRequest {requestHeaders = [(hUserAgent, userAgent)]}
-      parts          = [partFile fileKey fileName, partBS expiracyKey expiration]
-  request  <- formDataBody parts requestHeaders
+  let parts = [partFile fileKey fileName, partBS expiracyKey expiration, partBS reqKey reqType]
+  request <- formDataBody parts initialRequest
 
+  -- handle response
   response <- httpLbs request manager
-  putStrLn $ "The status code was: " ++ show (responseStatus response)
-  print $ responseBody response
+  let status = statusCode $ responseStatus response
+  if status /= 200 
+    then return $ Left $ errorMessage ++ show status
+    else let link       = T.strip $ T.pack $ BLI.unpackChars $ responseBody response 
+             linkString = T.unpack link
+         in return $ Right linkString
