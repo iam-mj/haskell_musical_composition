f97c7a66cab7a032b47b874ce5f2f446b29f0d99
Maria Verdeș <mariaverdes14@gmail.com>
2025-06-08 15:28:16 +0300

    fix: sans-serif font for html-midi-player

--- src/static/script.js
+++ src/static/script.js
@@ -3,8 +3,6 @@ const params   = new URLSearchParams(window.location.search);
 const midiURL  = params.get("src");
 const fileName = params.get("name"); 
 
-console.log("Got the parameter: ", midiURL, " & name: ", fileName);
-
 // set the player source
 document.getElementById("player").src = midiURL;
 document.getElementById("title").innerText = "File: " + fileName; 
